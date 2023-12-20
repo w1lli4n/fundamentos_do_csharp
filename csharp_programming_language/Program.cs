dcd7039a506077607d3e5a5672043f1638c8506d
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2023-12-20 14:44:21 -0300

    Terminado o módulo Linguagem de Programação C#. Apresentou a sintaxe básica.

--- csharp_programming_language/Program.cs
+++ csharp_programming_language/Program.cs
@@ -2,10 +2,13 @@
 // Console.WriteLine("Hello, World!");
 using System;
 
-namespace CsharpProgrammingLanguage{
-    class Program{
-        static void Main(string[] args){
-            
+namespace CsharpProgrammingLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
             // Variaveis
 
             int idadeDoGuri; // Estilo de declaração em C, além de utilizar camelCase :)
@@ -37,7 +40,7 @@ namespace CsharpProgrammingLanguage{
             long sampleLong = 64;
 
             Console.WriteLine($"short é um inteiro de {sampleShort} bit. int é um inteiro de {sampleInt} bit. long é um inteiro de {sampleLong} bit. Podem ser unsigned :)");
-        
+
             // reais
 
             float sampleFloat = 32f;
@@ -63,7 +66,7 @@ namespace CsharpProgrammingLanguage{
             string sampleString = "Um array de char";   // Será que funciona igual a de C?
 
             Console.WriteLine($"{sampleString}. Necessário ver detalhes de implementação.");
-        
+
             // var
 
             var sampleVar = "permite a inferencia do tipo em tempo de compilação";
@@ -82,10 +85,13 @@ namespace CsharpProgrammingLanguage{
 
             Console.WriteLine($"sampleNull está nula: {sampleNull}");
             sampleNull = "NotNull";
-           
-            if(sampleNull != null){
+
+            if (sampleNull != null)
+            {
                 Console.WriteLine($"sampleNull não é mais nula: {sampleNull}.");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("Null.");
             }
 
@@ -142,8 +148,166 @@ namespace CsharpProgrammingLanguage{
             Console.WriteLine($"sampleConvertBool veio da string \"True\", porém agora é {!sampleConvertBool}.");
 
             // Convert arredonda números inteiros
+
+            // Operadores
+
+            // Operadores aritimeticos
+
+            /* 
+                soma (+),
+                subtração (-),
+                multiplicação (*),
+                divisão (/)
+            */
+
+            // Podem ser utilizados com qualquer número. Divisões reais com resultados inteiros serão arredondados. Matematica :)
+
+            // Operadores de atribuição
+
+            /*
+                igual ( x = y),
+                mais igual ( x += y || x = x + y),
+                menos igual ( x -= y),
+                multiplicado igual ( x *= y),
+                dividido igual ( x /= y)
+            */
+
+            // Operadores de comparação
+
+            // sempre retorna um booleano
+            /*
+                igual (==),
+                diferente (!=),
+                maior-que (>),
+                menor-que (<),
+                maior-ou-igual-que (>=),
+                menor-ou-igual-que (<=),
+            */
+
+            // Operadores lógicos
+
+            /*
+                E (&&),
+                Ou (||),
+                Negação (!)
+            */
+
+            // Switch case. Basicamente o de C
+
+            int sampleSwitch = 1;
+
+            switch (sampleSwitch)
+            {
+                case 1:
+                    Console.WriteLine(true);
+                    break;
+                case 2:
+                    Console.WriteLine(false);
+                    break;
+                default:
+                    Console.WriteLine("maybe");
+                    break;
+            }
+
+            // Laços de repetição
+
+            // For
+
+            for (int i = 0; i < 5; i++)
+            {
+                System.Console.Write("+");
+                for (int j = 5; j > 0; j--)
+                {
+                    System.Console.Write("*");
+                }
+            }
+
+            // while
+
+            bool check = true;
+            while (check)
+            {
+                System.Console.WriteLine("true");
+                check = false;
+            }
+
+            // do while
+
+            do
+            {
+                System.Console.WriteLine("true?");
+            } while (check);
+            
+            // Funções ou Métodos (POO!)
+
+            /*
+                Não pode existir métodos dentro de métodos em C#
+            */
+
+            sampleMethod("Hello world?");
+
+            System.Console.WriteLine(fullName("Alves", "Willian", "dos Santos Almeida"));
+            System.Console.WriteLine(fullName("Borges", "Gabriela"));
+
+            // Value & references
+
+            /*
+                Todos os build in types são value types. Povoam a memória stack e são copiaveis.
+                Demais tipos, como arrays e objetos povoam a memória heap e apenas copiam a referência.
+            */
+            int x = 25;
+            int y = x;  // y é uma cópia de x, as duas variaveis são independentes
+
+            var arr = new string[2];
+            arr[0] = "ABC";
+            var arr2 = arr; // arr2 é apenas uma referencia de arr
+            System.Console.WriteLine($"{arr[0]} e {arr2[0]}");
+
+            arr2[0] = "CBA";    // Uma alteração em qualquer um dos dois irá alterar o outro. Ambos apontam para o mesmo espaço de memória.
+            System.Console.WriteLine($"{arr[0]} e {arr2[0]}");
+
+            // Structs
+
+            var mouse = new Product(1, "Mouse Gamer", 400, EImportado.Nacional);
+
+            System.Console.WriteLine($"Id do produto {mouse.Id}\nNome do produto: {mouse.Title}\nPreço do produto: R${mouse.Price:0.##}\nPreço em dolar: ${mouse.PriceInDolar(5.70):0.##}\nOrigem: {mouse.Origem}");
+
+        }
+
+        static void sampleMethod(string parametro)
+        {
+            System.Console.WriteLine(parametro);
+        }
+
+        static string fullName(string lastName, string name, string middleName = "")
+        {
+            return $"{name} {middleName} {lastName}";
         }
     }
+
+    struct Product
+    {
+        public Product(int id, string title, double price, EImportado origem)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+            Origem = origem;
+        }
+
+        public int Id;
+        public string Title;
+        public double Price;
+        public EImportado Origem;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price / dolar;
+        }
+    }
+
+    enum EImportado {   // enums de c# não são como as de rust, ou pelo menos não parece que são :(
+        Nacional = 1,
+        Importado = 2,
+    }
 }
-
-
