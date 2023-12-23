e925873408201e5054e88d102edda5d9e8aea96f
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2023-12-23 11:28:39 -0300

    Criado um meno para acessar os algoritimos. Ainda preciso melhorar, mas é uma primeira versão

---
+++ Algorithm/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Algo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Selecione o algoritimo desejado: ");
+
+      short counter = 1;
+      
+      try
+      {
+        foreach (string line in File.ReadLines("programs"))
+        {
+          Console.WriteLine($"[{counter}] {line}");
+          counter++;
+        }
+
+        var response = Console.ReadLine();
+
+        if (!short.TryParse(response, out short check) || check < 1 || check >= counter)
+        {
+          Console.WriteLine("Opição inválida!");
+          System.Environment.Exit(1);
+        }
+
+        string algo = File.ReadLines("programs").Skip(check - 1).Take(1).First();
+      
+        MethodInfo? method = typeof(Program).GetMethod(algo, BindingFlags.Public | BindingFlags.Static);
+
+        if(method != null)
+        {
+          method.Invoke(null, null);
+        } 
+        else
+        {
+          Console.WriteLine("Algoritimo não encontrado");
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Erro inesperado! {ex.Message}");
+      }
+    }
+
+    public static void Sample()
+    {
+      Console.WriteLine("Hello world!");
+    }
+  }
+}
