8c3d626b27d0eb707b759083291b7fe274d44b21
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2023-12-27 12:14:22 -0300

    Exceptions são interessantes e uma boa forma de error handling, porém acho a de rust superior

---
+++ Exceptions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3 };
+
+        try // Se ocorrer um erro dentro do bloco try, o programa muda seu curso para o bloco catch que possuir correspondecia
+        {
+            /*for (short i = 0; i < 10; i++)
+            {
+                Console.WriteLine(array[i]);
+            }*/
+
+            Salvar("");
+        }
+        catch (IndexOutOfRangeException ex)    // ex é do tipo exception, uma excessão generica, existem especificos como o IndexOutOfRangeException
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (MinhaException ex)
+        {
+            Console.WriteLine("error");
+            Console.WriteLine(ex.QuandoAconteceu);
+        }
+        catch
+        {
+            Console.WriteLine(":(");
+        }
+        finally     // Após a excessão, o programa se volta para o bloco finally
+        {
+            Console.WriteLine("Sempre serei executado");
+        }
+
+    }
+
+    private static void Salvar(string texto)
+    {
+        if (string.IsNullOrEmpty(texto))
+        {
+            // throw new Exception("O texto não pode ser vazio");   // generico
+            throw new MinhaException(DateTime.Now);
+        }
+    }
+
+    public class MinhaException : Exception
+    {
+        public MinhaException (DateTime time)
+        {
+            QuandoAconteceu = time;
+        }
+
+        public DateTime QuandoAconteceu { get; set; }
+    }
+}
