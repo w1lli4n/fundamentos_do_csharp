e818d7bdd171aa4a2bf710534b6562de4fa3a4e0
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2023-12-27 11:20:19 -0300

    moedas.

---
+++ moedas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // decimal é o ideal paara moedas
+        decimal valor = 112939.997m;
+
+        Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-br"))); // C de currency. Ele arredonda quando o valor passa de três casa decimais
+
+        Console.WriteLine($"valor real: {valor}; valor arredondado: {Math.Round(valor)}; valor arredondado pra cima: {Math.Ceiling(valor)}; valor arredondado pra baixo: {Math.Floor(valor)}");
+    }
+}
